1fc505262b80cfb6afb69d6853d9a49067bebb01
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2022-11-28 23:14:01 +0100

    chore: add storybook

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from "@storybook/vue3-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+};
+
+export default config;
---
+++ frontend/.storybook/preview.ts
@@ -0,0 +1,30 @@
+import "@/styles/index.scss";
+
+import { setup } from "@storybook/vue3";
+import i18n from "../src/i18n";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  options: {
+    storySort: {
+      order: [
+        "atoms",
+        "molecules",
+        "organisms",
+        "layouts",
+        "templates",
+        "views",
+      ],
+    },
+  },
+};
+
+setup((app) => {
+  app.use(i18n);
+});
