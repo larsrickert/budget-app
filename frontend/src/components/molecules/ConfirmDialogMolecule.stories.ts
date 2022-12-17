a6aa2df6acdb4959bcefec00e192066872e962cd
Lars Rickert <dev@lars-rickert.de>
2022-12-17 12:56:46 +0100

    add ConfirmDialogMolecule

---
+++ frontend/src/components/molecules/ConfirmDialogMolecule.stories.ts
@@ -0,0 +1,44 @@
+import type { VueProps } from "@/types/vue";
+import type { Meta, StoryObj } from "@storybook/vue3";
+import Component from "./ConfirmModalMolecule.vue";
+
+type Args = VueProps<typeof Component>;
+
+export default {
+  component: Component,
+} as Meta<Args>;
+
+export const Primary: StoryObj<Args> = {
+  args: {
+    modelValue: true,
+    title: "This is the title",
+    cancelText: "Cancel",
+    confirmText: "Confirm",
+    description: "Are you sure you want to do action XYZ?",
+  },
+};
+
+export const WithCustomSlot: StoryObj<Args> = {
+  args: {
+    ...Primary.args,
+  },
+  render: (args) => ({
+    components: { Component },
+    setup: () => ({ args }),
+    template: `<Component v-bind="args">Custom slot content.</Component>`,
+  }),
+};
+
+export const Disabled: StoryObj<Args> = {
+  args: {
+    ...Primary.args,
+    disabled: true,
+  },
+};
+
+export const Loading: StoryObj<Args> = {
+  args: {
+    ...Primary.args,
+    loading: true,
+  },
+};
---
+++ frontend/src/components/molecules/ConfirmDialogMolecule.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import { useVModel } from "@vueuse/core";
+import { ElButton, ElDialog } from "element-plus";
+
+const props = defineProps<{
+  title: string;
+  /** will be overridden when slot is used */
+  description?: string;
+  modelValue?: boolean;
+  cancelText: string;
+  confirmText: string;
+  disabled?: boolean;
+  loading?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (event: "update:modelValue", value: boolean): void;
+  (event: "confirm", close: () => void): void;
+}>();
+
+const isOpen = useVModel(props, "modelValue", emit);
+
+const handleConfirm = () => {
+  emit("confirm", () => (isOpen.value = false));
+};
+</script>
+
+<template>
+  <el-dialog
+    v-model="isOpen"
+    :title="title"
+    width="30%"
+    align-center
+    destroy-on-close
+    :show-close="!loading"
+    :close-on-click-modal="!loading"
+    :close-on-press-escape="!loading"
+  >
+    <slot>{{ description }}</slot>
+
+    <template #footer>
+      <el-button @click="isOpen = false" :disabled="loading">
+        {{ cancelText }}
+      </el-button>
+      <el-button
+        type="primary"
+        @click="handleConfirm"
+        :disabled="disabled"
+        :loading="loading"
+      >
+        {{ confirmText }}
+      </el-button>
+    </template>
+  </el-dialog>
+</template>
+
+<style lang="scss" scoped></style>
