d883a87cd622915421a2f3ab314613337be6c688
Lars Rickert <dev@lars-rickert.de>
2022-12-17 13:22:22 +0100

    add FileUploadOrganism

---
+++ frontend/src/components/organisms/FileUploadOrganism.stories.ts
@@ -0,0 +1,44 @@
+import type { VueProps } from "@/types/vue";
+import type { Meta, StoryObj } from "@storybook/vue3";
+import Component from "./FileUploadOrganism.vue";
+
+type Args = VueProps<typeof Component>;
+
+export default {
+  component: Component,
+} as Meta<Args>;
+
+export const Primary: StoryObj<Args> = {
+  args: {},
+};
+
+export const Disabled: StoryObj<Args> = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const WithLimit: StoryObj<Args> = {
+  args: {
+    limit: 1,
+  },
+};
+
+export const WithReplace: StoryObj<Args> = {
+  args: {
+    limit: 2,
+    replace: true,
+  },
+};
+
+export const WithAllowedFileTypes: StoryObj<Args> = {
+  args: {
+    allowedFileTypes: [".jpg", ".png"],
+  },
+};
+
+export const WithMaxSize: StoryObj<Args> = {
+  args: {
+    maxSize: 4,
+  },
+};
