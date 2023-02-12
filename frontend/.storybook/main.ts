a3b96f7bb1601aceb8e2a0f2874ea50cca7e4e95
Lars Rickert <dev@lars-rickert.de>
2023-02-12 15:28:16 +0100

    chore: migrate to pnpm (#19)

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -2,15 +2,8 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/vue3-vite",
-    options: {},
-  },
+  addons: ["@storybook/addon-essentials"],
+  framework: "@storybook/vue3-vite",
   core: {
     disableTelemetry: true,
   },
