fe68c682c012b9abb654551592fc9763599e907c
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2023-05-29 12:58:20 +0200

    chore: update dependencies

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-essentials"],
   framework: "@storybook/vue3-vite",
   core: {
--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,38 +1,43 @@
 import "@/styles/index.scss";
+import "element-plus/theme-chalk/dark/css-vars.css";
 
+import type { Preview } from "@storybook/vue3";
 import { setup } from "@storybook/vue3";
 import { ElLoading } from "element-plus";
-import "element-plus/theme-chalk/dark/css-vars.css";
 import i18n from "../src/i18n";
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
-  },
-  options: {
-    storySort: {
-      order: [
-        "atoms",
-        "molecules",
-        "organisms",
-        "layouts",
-        "templates",
-        "views",
+    options: {
+      storySort: {
+        order: [
+          "atoms",
+          "molecules",
+          "organisms",
+          "layouts",
+          "templates",
+          "views",
+        ],
+      },
+    },
+    backgrounds: {
+      default: "light",
+      values: [
+        { name: "light", value: "#fff" },
+        { name: "dark", value: "#141414" },
       ],
     },
   },
-  backgrounds: {
-    default: "light",
-    values: [
-      { name: "light", value: "#fff" },
-      { name: "dark", value: "#141414" },
-    ],
-  },
 };
+
+export default preview;
 
 setup((app) => {
   app.use(i18n).use(ElLoading);
