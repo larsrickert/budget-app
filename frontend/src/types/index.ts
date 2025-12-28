13cdbf7f843085342107106afb4410c9afcebc41
Lars Rickert <dev@lars-rickert.de>
2025-12-28 16:18:54 +0100

    delete all current files

--- frontend/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-import type { ElButton } from "element-plus";
-import type { InjectionKey, Ref } from "vue";
-import type { VueProps } from "./vue";
-
-export const provideIsDrawerOpenSymbol: InjectionKey<Ref<boolean>> = Symbol();
-
-export type Icon = VueProps<typeof ElButton>["icon"];
